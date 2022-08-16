b5ba61067e86370bec154a69043b17a51d62728a
A.S <37290813+Skwangles@users.noreply.github.com>
2022-08-17 09:42:45 +1200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,36 @@
-  GNU nano 4.8                                          index.js
 const express = require('express'); 
 const mysql = require('mysql');
 require('dotenv').config();  
-const bodyParser = require('body-parser');      
+const util = require('util');
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: process.env.sqluser,
+  password: process.env.sqlpass,
+  database: "waikato_db"
+});
+
 const app = express();                   
-//app.use(bodyParser.urlencoded({extended:false}));     
-//app.use(bodyParser.json());              
+              
 app.use(express.json());
-app.post('/', (request, response) => {    
+app.get('/', (request, response) => {    
   console.log(request.headers);  
-  console.log(request.body);   
-  response.end("Received message " + request);  
+  console.log(request.query);   
+ try{
+   let item = await query(request.query.query);//Forwards WITHOUT checking for injection - yes, we know.
+   console.log(item);
+   response.end(JSON.stringify(item));
+   return;
+ }
+  catch(err){
+   response.end(JSON.stringify(err));
+    return;
+  }
+  response.end("Somehow the try/catch got skipped?");
 });                
 
-app.listen(3000, ()=>{console.log("Listening on 3000")});    
+coon.connect((err)=>{
+ if (err) throw err;
+  console.log("Connected!");
+});
+
+app.listen(3000, ()=>{console.log("Listening on port 3000")});    
