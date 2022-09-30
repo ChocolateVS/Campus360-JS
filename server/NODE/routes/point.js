a43fbe32d903bfe42208afe58b848fe6580b2b27
A.S <37290813+Skwangles@users.noreply.github.com>
2022-09-30 15:19:10 +1300

    Add files via upload

---
+++ server/NODE/routes/point.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const util = require('util');
+const router = express.Router();
+
+//Mongo
+const {ObjectId} = require('mongoose').Types
+const {Point, Level} = require('../models/schema.js');
+const { getProject, getArea, getLevel, getPoint } = require('../shared.js');
+
+let tableName = "Point";
+
+router.get('/:pointId', getProject, getArea, getLevel, getPoint, async(req, res) => {
+    res.status(200).json({ success: true, payload: res.point })
+});
+
+router.get('/', getProject, getArea, getLevel, (req, res, next) => {
+    res.status(200).json({success:true, payload: res.level.points})
+});
+
+router.post('/', getProject, getArea, getLevel, async (req, res) => {
+    console.log(req.params)
+    let newPoint;
+    let createObj = {};
+    if(req.query.id) createObj._id = ObjectId(req.query.id)
+    //Levels have specific referencing, so can only be added via /level API
+    if(req.query.x) createObj.x = req.query.x;
+    if(req.query.y) createObj.y = req.query.y;
+    if(req.query.gyro_data) createObj.gyro_data = req.query.gyro_data;
+    if(req.query.name) createObj.name = req.query.name;
+    if(res.level){
+        newPoint = new Point(createObj);
+        res.level.points.push(newPoint._id)
+    }
+    try{
+        //Save changes to DB
+        if(newPoint == null) throw 'Could not create Point object';
+        const saveResult = await newPoint.save();
+        await res.level.save();
+        res.status(201).json({success:true, payload: saveResult})
+    }
+    catch(err){
+        res.status(400).json({success:false, message: err.message})
+    } 
+});
+
+router.patch('/:pointId', getProject, getArea, getLevel, getPoint, async (req, res) => {
+    try{
+        let resp = await res.point.updateOne({_id: ObjectId(req.params.pointId)}, {$set: req.query});//Only updates the attributes specified in 'query'
+        res.status(200).json({ success: true, payload: resp }).end();
+        }
+        catch(ex){
+            res.status(400).json({"success":false, message: err.message}).end()
+        }
+});
+
+router.delete('/:pointId', getProject, getArea, getLevel, getPoint, async(req, res) => {
+    try{
+        //remove references
+        await Level.updateMany({'points': res.point._id},{$pull: {points: res.point._id}});
+        await Point.updateMany({'links': res.point._id},{$pull: {links: res.point._id}});
+        await Level.updateMany({'rooms.points': res.point._id},{$pull: {rooms: {points: res.point._id}}});
+
+        let deleteResponse = await res.point.deleteOne();
+        res.status(200).json({ success: true, payload: deleteResponse }).end();
+    }
+    catch (err){
+        res.status(500).json({"success":false, message: err.message}).end()
+    }
+});
+
+
+
+module.exports = router;
