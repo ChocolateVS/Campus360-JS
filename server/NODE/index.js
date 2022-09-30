f43643db13c7b0a9c40861490ab2bc579ab5fe89
Skwangles <tree_house@hotmail.co.nz>
2022-09-30 14:27:35 +1300

    Moved files to correct location

--- server/NODE/index.js
+++ server/NODE/index.js
@@ -1,16 +1,35 @@
-const express = require('express');
-const multer = require('multer');
 require('dotenv').config();
-const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const api = require('./api.js');
+const api = require('./routes/api.js');
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const url = process.env.MONGO_URL || "mongodb://localhost:27017/waikato_db";
+
+
+
+//API urls
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/api', api);
 app.use('/', express.static(__dirname + '/Campus360-JS'));
 
-let port = process.env.PORT ? process.env.PORT : 3000;
-app.listen(port, ()=>{console.log("Listening on " + port)});
+
+mongoose.connect(url,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connection to database established...')
+    
+    let port = process.env.PORT ? process.env.PORT : 3000;
+    app.listen(port, ()=>{console.log("Listening on " + port)});
+
+  }).catch(err => {
+    console.log(err);
+  });
+
+
